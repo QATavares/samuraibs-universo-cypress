adf40a2fbda82b9992d11e8d420acc328c9d8d78
Rodrigo Tavares <rtsantana@outlook.com.br>
2022-11-18 15:10:10 -0300

    creating test mass independent of user data changes

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -8,8 +8,24 @@ describe('login', function(){
     const user = {
       name: 'Peter Parker',
       email: 'spiderman@samuraibs.com',
-      password: 'pwd1234'
+      password: 'pwd1234',
+      is_provider: true
      }
+
+     before(function(){
+      cy.task('removeUser', user.email)
+        .then(function(result){
+          console.log(result)
+        })
+
+      cy.request(
+          'POST',
+          'http://localhost:3333/users',
+          user)
+        .then(function (response){
+          expect(response.status).to.eq(200)
+        })
+      })
 
     it('deve logar com sucesso', function(){
       loginPage.go()
